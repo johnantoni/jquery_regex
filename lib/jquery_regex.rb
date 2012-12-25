ab595608e15098d3225ebef8570bb64ef2b76aa4
John Griffiths <john@johnantoni.com>
2012-12-25 00:04:20 -0500

    init

---
+++ lib/jquery_regex.rb
@@ -0,0 +1,8 @@
+require "jquery_regex/version"
+
+module JqueryRegex
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ lib/jquery_regex/version.rb
@@ -0,0 +1,3 @@
+module JqueryRegex
+  VERSION = "0.0.1"
+end
